b2e709d96d0cb8c289e85bca95f7b23413a23725
Zhen-hao <zhenhaolee@gmail.com>
2023-10-21 17:28:43 +0200

    add gitignore

--- plugin/src/main/scala/se/nullable/sbtix/CompositionWriter.scala
+++ plugin/src/main/scala/se/nullable/sbtix/CompositionWriter.scala
@@ -7,6 +7,14 @@ object CompositionWriter {
          |# this file originates from SBTix
          |{ pkgs ? import <nixpkgs> {}
          |, cleanSource ? pkgs.lib.cleanSource
+         |, gitignore ? (let
+         |                 src = pkgs.fetchFromGitHub { 
+         |                   owner = "hercules-ci";
+         |                   repo = "gitignore.nix";
+         |                   rev = "9e21c80adf67ebcb077d75bd5e7d724d21eeafd6";
+         |                   sha256 = "sha256:vky6VPK1n1od6vXbqzOXnekrQpTL4hbPAwUhT5J9c9E=";
+         |                 };
+         |               in import src { inherit (pkgs) lib; })
          |}:
          |
          |let
@@ -14,7 +22,7 @@ object CompositionWriter {
          |in
          |  sbtix.buildSbt${compositionType.capitalize} {
          |    name = "$buildName";
-         |    src = cleanSource ./.;
+         |    src = cleanSource (gitignore.gitignoreSource ./.);
          |    #sbtixBuildInputs = pkgs.callPackage ./sbtix-build-inputs.nix {};
          |    repo = [
          |      (import ./repo.nix)
--- tests/multi-build/two/default.nix
+++ tests/multi-build/two/default.nix
@@ -1,5 +1,15 @@
 { pkgs ? import <nixpkgs> {}
 , cleanSource ? pkgs.lib.cleanSource
+, gitignore ? (let
+                  src = pkgs.fetchFromGitHub { 
+                    owner = "hercules-ci";
+                    repo = "gitignore.nix";
+                    # put the latest commit sha of gitignore Nix library here:
+                    rev = "9e21c80adf67ebcb077d75bd5e7d724d21eeafd6";
+                    # use what nix suggests in the mismatch message here:
+                    sha256 = "sha256:vky6VPK1n1od6vXbqzOXnekrQpTL4hbPAwUhT5J9c9E=";
+                  };
+                in import src { inherit (pkgs) lib; })
 }:
 
 let
